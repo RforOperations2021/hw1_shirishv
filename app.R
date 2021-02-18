9cbadb6bdf6fc08d3459f16baabebdb50a134cd9
shirishverma <70457478+shirishverma@users.noreply.github.com>
2021-02-18 18:09:59 -0500

    Added 2 more plots and merged the functionalities of plot 1 and 2. Need to debug plot 1 code.

--- app.R
+++ app.R
@@ -27,41 +27,75 @@ ui <- fluidPage(
         # Inputs: Select variables to plot
         sidebarPanel(
             
-            # Select indicators for y axis
-            selectInput(inputId = "y", 
+            h4(strong("Plot 1 & 2 functionalities")),
+            br(),
+            
+            # Select indicators for y axis of Plot 1 and 2
+            selectInput(inputId = "y1",
                         label = "Y-axis:",
                         choices = colnames(world.bank.df)[5:50],
                         selected = "Population.growth..annual..."),
             
-            # Select years for x-axis
-            sliderInput(inputId = "x", 
+            # Select years for x-axis of Plot 1 and 2
+            sliderInput(inputId = "x1", 
                   label = "X-axis:", 
                   min = 2000, 
                   max = 2015,
                   step = 1, 
-                  value = c(2000,2015)),
-            
-            # Show data table
-            checkboxInput(inputId = "show_data",
-                          label = "Show data table",
-                          value = TRUE),
+                  value = c(2010,2015)),
             
             # Horizontal line for visual separation
             hr(),
             
             # Select which types of movies to plot ------------------------
             checkboxGroupInput(inputId = "continent",
-                               label = "Select continent(s):",
+                               label = "Select continent(s)*:",
                                choices = unique(world.bank.df$ContinentName),
-                               selected = "North America")
+                               selected = "North America"),
+            h6("* applicable everywhere except plot 1"),
+            
+            # Horizontal line for visual separation
+            hr(),
+            h4(strong("Plot 3 functionalities")),
+            br(),
+            
+            # Select the country on focus for Plot 3
+            uiOutput("selectCountry"),
+            #selectInput(inputId = "country",
+            #         label = "Select a country:",
+            #         choices = world.bank.df$CountryName),
+            
+            # Select an indicator for y axis of Plot 3
+            selectInput(inputId = "y3",
+                        label = "Y-axis:",
+                        choices = colnames(world.bank.df)[5:50],
+                        selected = "Exports.of.goods.and.services....of.GDP."),
+            
+            # Select an indicator for x axis of Plot 3
+            selectInput(inputId = "x3",
+                        label = "X-axis:",
+                        choices = colnames(world.bank.df)[5:50],
+                        selected = "Imports.of.goods.and.services....of.GDP."),
+            hr(),
+            # Show data table
+            checkboxInput(inputId = "show_data",
+                          label = "Show data table",
+                          value = TRUE)
+            
         ),
 
         # Output
         mainPanel(
             # Show lineplot
-            plotOutput(outputId = "lineplot", hover = hoverOpts(id = "plot_hover", delay = 100)),
-            verbatimTextOutput("info"),
+            plotOutput(outputId = "barplot"),
             br(),
+            hr(),
+            plotOutput(outputId = "lineplot"),
+            br(),
+            hr(),
+            plotOutput(outputId = "scatterplot"),
+            br(),
+            hr(),
             
             # Show data table
             DT::dataTableOutput(outputId = "table")
@@ -72,7 +106,7 @@ ui <- fluidPage(
 # Define server logic required to draw plots and tables
 server <- function(input, output) {
     
-    # setwd(dir = "C:/Users/shiri/Documents/Homework_1/Economic_indicators")
+    setwd(dir = "C:/Users/shiri/Documents/Homework_1/Economic_indicators/hw1_sverma")
     # Read the dataset
     world.bank.df <- read.csv("world_bank_data.csv", check.names = FALSE, na.strings = "..")
     colnames(world.bank.df)[1] <- "IndicatorName"
@@ -97,28 +131,73 @@ server <- function(input, output) {
     
     world.bank.df$Year <- as.numeric(as.character(world.bank.df$Year))
     
+    # Make a subset of aggregated values based on the idicator selected for Plot 1
+    indicator_subset <- reactive({
+        req(input$y1)
+        world.bank.df %>%
+            group_by(ContinentName, Year) %>%
+            summarise(Mean = mean(input$y1, na.rm = TRUE))
+    })
+    
+    # Create a barplot making comparisons within continents on the selected indicator
+    output$barplot <- renderPlot({
+        ggplot(data = indicator_subset(), aes_string(x = "Year", 
+                                                     y = "Mean", 
+                                                     fill = "ContinentName")) +
+            geom_bar(stat = "identity", width = 0.5, position = "dodge") +
+            scale_x_continuous(breaks = seq(input$x1[1],input$x1[2],1), 
+                               limits = c(input$x1[1],input$x1[2])) +
+            scale_y_continuous(labels = scales::comma) +
+            labs(x = "Year", y = paste("Indicator: ", input$y1), 
+                 title = paste0("Plot 1: Performance of continents on aggregate level from ", 
+                                input$x1[1], " to ", input$x1[2])) +
+            theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
+    })
+    
     # Create a subset of data filtering for selected continents
     continents_subset <- reactive({
         req(input$continent)
         filter(world.bank.df, ContinentName %in% input$continent)
     })
-   
+    
+    # Create a lineplot between some development indicator and years
     output$lineplot <- renderPlot({
         ggplot(data = continents_subset(), aes_string(x = "Year", 
-                                               y = input$y,
+                                               y = input$y1,
                                                color = "CountryName")) +
             geom_line() +
-            scale_x_continuous(breaks = seq(input$x[1],input$x[2],1), limits = c(input$x[1],input$x[2])) +
+            scale_x_continuous(breaks = seq(input$x1[1],input$x1[2],1), 
+                               limits = c(input$x1[1],input$x1[2])) +
             scale_y_continuous(labels = scales::comma) +
-            labs(x = "Year", y = paste("Indicator: ", input$y), 
-            title = paste(paste(paste("Trend in countries indicators from ", input$x[1]), " to "), input$x[2])) +
+            labs(x = "Year", y = paste("Indicator: ", input$y1), 
+            title = paste0("Plot 2: Trend in countries indicators from ", 
+                           input$x1[1], " to ", input$x1[2])) +
             theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
     })
     
-    output$info <- renderText({
-        paste0("Year = ", input$plot_hover$x, "\nIndicator value = ", input$plot_hover$y)
+    # Crete an output UI which can show filtered country names in dropdown
+    output$selectCountry <- renderUI(
+        selectInput(inputId = "country",
+                 label = "Select a country:",
+                 choices = continents_subset()$CountryName,
+                 selected = "")
+    )
+    
+    # Create a subset of data filtering for selected continents
+    country_subset <- reactive({
+        req(input$country)
+        filter(world.bank.df, CountryName %in% input$country)
     })
     
+    # Create a scatterplot between 2 development indicators for a selected country
+    output$scatterplot <- renderPlot({
+        ggplot(data = country_subset(), aes_string(x = input$x3, y = input$y3, label = "Year")) +
+            geom_point(size = 5,color = "orange") +
+            geom_text(aes(label = Year), hjust = 0, vjust = -1) +
+            labs(x = input$x3, y = input$y3, 
+                 title = paste0("Plot 3: Comparison between two indicators between 2000 and 2015 for ", input$country)) +
+            theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
+    })
     
     # Print data table if checked
     output$table <- DT::renderDataTable(
